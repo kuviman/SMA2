5cd45b796414bb9cffc5857eb41e9139e52a0fb2
kuviman <kuviman@gmail.com>
2013-11-09 01:33:08 +0400

    Player.
    Added empty player class.

---
+++ SMA2/Source/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using VitPro.Engine;
+
+namespace VitPro.SMA2 {
+
+	class Player : IRenderable, IUpdateable {
+
+		public Vec2 Position = Vec2.Zero;
+		public double Size = 1;
+
+		public void Update(double dt) {
+		}
+
+		static Texture texture = new Texture("../Data/Player.png");
+
+		public void Render() {
+			Draw.Save();
+			Draw.Translate(Position);
+			Draw.Scale(Size * 2);
+			Draw.Align(0.5, 0.5);
+			texture.Render();
+			Draw.Load();
+		}
+
+	}
+
+}
--- SMA2/Source/World.cs
+++ SMA2/Source/World.cs
@@ -8,18 +8,25 @@ namespace VitPro.SMA2 {
 		Camera cam = new Camera(20);
 		Texture back = new Texture("../Data/Back.png");
 
+		public Player player = new Player();
+
 		public void Update(double dt) {
+			player.Update(dt);
 		}
 
 		public void Render() {
 			Draw.Save();
 			cam.Apply();
+
 			Draw.Save();
 			Draw.Scale(20);
 			Draw.Scale((double)back.Width / back.Height, 1);
 			Draw.Align(0.5, 0.5);
 			back.Render();
 			Draw.Load();
+
+			player.Render();
+
 			Draw.Load();
 		}
 
