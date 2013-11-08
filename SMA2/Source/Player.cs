e517561b24c4b11cc352f34bf3c77921c0564f71
kuviman <kuviman@gmail.com>
2013-11-09 01:35:10 +0400

    Player movement.
    Player can move now.

--- SMA2/Source/Player.cs
+++ SMA2/Source/Player.cs
@@ -8,7 +8,19 @@ namespace VitPro.SMA2 {
 		public Vec2 Position = Vec2.Zero;
 		public double Size = 1;
 
+		const double Speed = 5;
+
 		public void Update(double dt) {
+			double vx = 0, vy = 0;
+			if (Key.A.Pressed())
+				vx -= 1;
+			if (Key.D.Pressed())
+				vx += 1;
+			if (Key.W.Pressed())
+				vy += 1;
+			if (Key.S.Pressed())
+				vy -= 1;
+			Position += new Vec2(vx, vy).Unit * Speed * dt;
 		}
 
 		static Texture texture = new Texture("../Data/Player.png");
