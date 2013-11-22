3010c89bea94ae87aef926543c0f15a0e5d24e4e
kuviman <kuviman@gmail.com>
2013-11-23 01:39:38 +0400

    Explosion better graphics.

--- SMA2/Source/Explosion.cs
+++ SMA2/Source/Explosion.cs
@@ -6,15 +6,27 @@ namespace VitPro.SMA2 {
 	class Explosion : SpaceObject {
 
 		public Explosion(Vec2 pos, double size)
-			: base(0.3) {
+			: base(0.15) {
 				Position = pos;
 				Size = size;
 				Collideable = false;
 		}
 
+		double swapT = 0;
+		double Rotation;
+
+		const double GrowSpeed = 2;
+		const double SwapTime = 0.05;
+
 		public override void Update(double dt) {
 			Health -= dt;
 			base.Update(dt);
+			Size *= (1 + GrowSpeed * dt);
+			swapT -= dt;
+			if (swapT < 0) {
+				Rotation = GRandom.NextDouble(0, 2 * Math.PI);
+				swapT = SwapTime;
+			}
 		}
 
 		static Texture tex = new Texture("../Data/Explosion.png");
@@ -24,7 +36,9 @@ namespace VitPro.SMA2 {
 			Draw.Save();
 			Draw.Translate(Position);
 			Draw.Scale(Size * 2);
+			Draw.Rotate(Rotation);
 			Draw.Align(0.5, 0.5);
+			Draw.Color(1, 1, 1, Health / MaxHealth);
 			tex.Render();
 			Draw.Load();
 		}
