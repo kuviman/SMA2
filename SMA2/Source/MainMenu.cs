d7045e75b16537f53d576bf614597d216ad38389
kuviman <kuviman@gmail.com>
2013-11-27 00:23:57 +0400

    Better menu.
    Now have clickable menu buttons.

--- SMA2/Source/MainMenu.cs
+++ SMA2/Source/MainMenu.cs
@@ -6,6 +6,51 @@ namespace VitPro.SMA2 {
 
 	class MainMenu : State {
 
+		class Button : IRenderable, IUpdateable {
+
+			string Text;
+			double y;
+			public Action Action;
+
+			public Button(string text, double y) {
+				this.Text = text;
+				this.y = y;
+			}
+
+			public void Update(double dt) {
+				dt *= 5;
+				if (Selected)
+					t = Math.Min(t + dt, 1);
+				else
+					t = Math.Max(t - dt, 0);
+			}
+
+			double t = 0;
+
+			public void Render() {
+				Draw.Save();
+				Draw.Translate(0, y);
+				Draw.Align(font.Measure(Text) / 2, 0);
+				Draw.Save();
+				Draw.Scale(font.Measure(Text), 1);
+				var backCol = new Color(0.6, 0.6, 1, t / 1.5);
+				Draw.Color(backCol);
+				Draw.Quad();
+				Draw.Load();
+				Draw.Color(0, 0, 0);
+				font.Render(Text);
+				Draw.Load();
+			}
+
+			public bool Selected = false;
+
+			public bool Inside(Vec2 pos) {
+				double sx = font.Measure(Text);
+				return Math.Abs(pos.X) < sx / 2 && pos.Y > y && pos.Y < y + 1;
+			}
+
+		}
+
 		World world = new World();
 
 		public MainMenu() {
@@ -13,11 +58,34 @@ namespace VitPro.SMA2 {
 			for (int i = 0; i < 300; i++) {
 				world.Update(0.2);
 			}
+			var b = new Button("PLAY", 0);
+			b.Action = () => StateManager.PushState(new Test());
+			buttons.Add(b);
+			b = new Button("exit", -3);
+			b.Action = () => StateManager.Close();
+			buttons.Add(b);
+		}
+
+		List<Button> buttons = new List<Button>();
+
+		public override void MouseMove(Vec2 pos) {
+			base.MouseMove(pos);
+			pos = new Camera(10).FromWH(pos, App.Width, App.Height);
+			foreach (var b in buttons)
+				b.Selected = b.Inside(pos);
+		}
+
+		public override void MouseDown(MouseButton button, Vec2 pos) {
+			base.MouseDown(button, pos);
+			foreach (var b in buttons)
+				if (b.Selected)
+					b.Action.Apply();
 		}
 
 		public override void Update(double dt) {
 			base.Update(dt);
 			world.Update(dt);
+			buttons.Update(dt);
 		}
 
 		public override void Render() {
@@ -36,21 +104,13 @@ namespace VitPro.SMA2 {
 			base.Render();
 
 			Draw.Save();
-			Draw.Color(0, 0, 0);
 			new Camera(10).Apply();
-			Draw.Translate(-5, 0);
-			font.Render("1 for new game");
-			Draw.Translate(0, -1);
-			font.Render("2 for exit");
+			buttons.Render();
 			Draw.Load();
 		}
 
 		public override void KeyDown(Key key) {
 			base.KeyDown(key);
-			if (key == Key.Number2)
-				Close();
-			else if (key == Key.Number1)
-				StateManager.PushState(new Test());
 		}
 
 		static SystemFont font = new SystemFont("Courier New", 64, FontStyle.Bold);
