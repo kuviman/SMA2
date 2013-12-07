5c2060ed192cb695339430e8df97ba09df8a8744
kuviman <kuviman@gmail.com>
2013-12-07 23:47:21 +0400

    MachineGun randomization.
    Machine gun is now not 100% accurate.

--- SMA2/Source/MachineGun.cs
+++ SMA2/Source/MachineGun.cs
@@ -6,7 +6,10 @@ namespace VitPro.SMA2 {
 	class MachineGun : Weapon {
 		public MachineGun() : base(0.1) { }
 		protected override void DoShoot(Vec2 pos) {
-			var dust = new Dust(Owner.Position + (pos - Owner.Position).Unit * Owner.Size * 1.5, 0.5);
+			const double acc = 0.1;
+			var dir = Vec2.Rotate((pos - Owner.Position).Unit, GRandom.NextDouble(-acc, acc));
+			pos = Owner.Position + dir * 100500;
+			var dust = new Dust(Owner.Position + dir * Owner.Size * 1.5, 0.5);
 			const double minspeed = 3, maxspeed = 5;
 			dust.Velocity = (pos - Owner.Position).Unit * GRandom.NextDouble(minspeed, maxspeed);
 			World.Current.Add(dust);
